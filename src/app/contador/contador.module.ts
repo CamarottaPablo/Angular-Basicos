46c6649648e94586460a0022f0046ab49e05555e
CamarottaPablo <83800798+CamarottaPablo@users.noreply.github.com>
2021-11-12 10:26:17 -0300

    Ruteo, Modularizacion, puesta en produccion netlify

--- src/app/contador/contador.module.ts
+++ src/app/contador/contador.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from "@angular/core";
 import { ContadorComponent } from './contador/contador.component';
 
-
-
-
-
 @NgModule({
     declarations: [
         ContadorComponent,
--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -1,13 +1,10 @@
 import { Component } from "@angular/core";
 
-
-
-
 @Component({
     selector: 'app-heroe',
     templateUrl: 'heroe.component.html'
 })
-export class HeroeComponent {
+export class HeroeComponent {   
 
     nombre: string = 'Ironman';
     edad  : number = 45;
